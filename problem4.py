ed051ee245a1ca373e1d9bd14c051f46ebe24ee7
Corrie <cgripens@gmail.com>
2015-05-14 21:57:26 -0700

    Improved solution

--- problem4.py
+++ problem4.py
@@ -1,10 +1,14 @@
-num = 0
-product = 0
+def palindrome(numDigits):
+    num = 0
+    product = 0
 
-for i in range(999):
-  for j in range(999):
-    product = i * j
-    if str(product) == str(product)[::-1] and product > num:
-        num = product
+    for i in range(10**(numDigits)-1):
+      for j in range(10**(numDigits)-1):
+        product = i * j
+        if str(product) == str(product)[::-1] and product > num:
+            num = product
 
-print(num)
+    return num
+
+result = palindrome(3)
+print(result)
